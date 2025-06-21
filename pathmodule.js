c566b55aefcd7818b2911a7ae61fd61e54d23f20
Crystal <crystall.101600@gmail.com>
2025-06-21 16:24:17 +0530

    Contains the functions of path module.

---
+++ pathmodule.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const a = path.basename('home\\crystal\\1.html');
+const b = path.dirname('home\\crystal\\1.html');
+const c = path.extname(__filename)
+console.log(a)
+console.log(b)
+console.log(__filename, c)
+
